33b42ec873465458beb945a385b6c3653ae703d3
muhib349 <muhibgazi@gmail.com>
2024-08-13 12:45:55 +0100

    added network client with sample code

---
+++ network/lib/rest/client/network_client.dart
@@ -0,0 +1,10 @@
+abstract class NetworkClient {
+  dynamic get(dynamic queryParam);
+  dynamic post(dynamic requestBody);
+  dynamic put(dynamic queryParam);
+  dynamic delete(dynamic queryParam);
+  dynamic multipart();
+
+  void addInterceptor();
+  void deleteInterceptor();
+}
